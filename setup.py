e5d6773530b94f0790a02c8252d7fc1314729f06
Sam Minton <sam@purecreative.digital>
2021-04-13 22:28:19 +1000

    Require Django 3.2 and Wagtail 2.12.3

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django>=3.2',
-        'wagtail>=2.9.3',
+        'wagtail>=2.12.3',
         'django-countries==5.5',
         'django-extensions==2.2.1',
         'djangorestframework==3.10.3',
